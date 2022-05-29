3270f303e75f85c244a11b8f1be2ab2f0c651768
Alebereto <1albertkhayat1@gmail.com>
2022-05-29 22:20:15 +0300

    Add clampAngle function
    Removed function from Player.cs

--- Assets/Scripts/VectorUtilities.cs
+++ Assets/Scripts/VectorUtilities.cs
@@ -11,4 +11,15 @@ public static class VectorUtilities
             
         );
     }   //Unsure if this angle is upwards
+
+    // Gets float value of an angle in radians, returns clamped angle
+    public static float clampAngle(float angle)
+    {
+        angle = angle % (2*Mathf.PI);
+
+        if (angle < 0)
+        {
+            return angle + (2f * Mathf.PI);
+        }
+        return angle;
 }
