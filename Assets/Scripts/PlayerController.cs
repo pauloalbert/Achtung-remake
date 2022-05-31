03507a82aee888944e1567616dcff457e8b77799
Alebereto <1albertkhayat1@gmail.com>
2022-05-31 21:29:45 +0300

    Change player script name
    having it just called player was annoying
    and this kind of script is usually called playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("Input")]
 
